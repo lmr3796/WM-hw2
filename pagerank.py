354b237535a14b1732086c879aa8bea63d67a109
lmr3796 <lmr3796joseph@gmail.com>
2013-05-21 21:51:43 +0800

    Implement transition

--- pagerank.py
+++ pagerank.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-import sys
+import sys, copy
 
 EPS=1e-6
 d=0.85
@@ -10,24 +10,47 @@ conf = {}
 class AdjacentGraph:
     def __init__(self, f):
         content = f.readlines()
-        self.data = {}
+        self.link = {}
         self.maxnode = int(content[0].split()[1])
         for line in content[1:]:
             (in_node, out_nodes) = line.split(':')
             # First number after : denotes the amount of outlinks
-            self.data[int(in_node)] = tuple([int(x) for x in out_nodes.split()[1:]])
+            self.link[int(in_node)] = tuple([int(x) for x in out_nodes.split()[1:]])
         return
 
-    def adjacent(self, i, j):
-        return (i in self.data) and (j in self.data[i])
+    def no_out_link(self, i):
+        return self.link
 
 class PageRank:
-    def __init__(self):
+    def __init__(self, maxnode):
+        self.maxnode = maxnode
+        # 1-based index
+        self.prestige = [1.0 / maxnode] * (maxnode + 1)
+        self.prestige[0] = 0.0
+        self.prestige = numpy.array(self.prestige)
         return
+
+    # Transition
+    def __mul__(self, A):
+        assert A.maxnode == self.maxnode
+        # Initiate with the 1-d for random surfing
+        new_prestige = numpy([1-d] * (self.maxnode + 1))
+        for i in range(1, len(new_prestige)):
+            if A.no_out_link(i):    # Share prestige to every one
+                new_prestige += d * self.prestige[i] / self.maxnode
+            else:
+                for j in A.link[i]: # Share prestige to linked ones
+                    new_prestige[j] += d * self.prestige[i] / len(A.link[i])
+
+        # Return a new Pagerank instance
+        result = Pagerank(self.maxnode)
+        result.prestige = new_prestige
+        return result 
 
 def main():
     with open(sys.argv[1]) as f:
-        adj_map = AdjacentGraph(f)
+        A = AdjacentGraph(f)
+    p = Pagerank(A.maxnode)
 
     return 0
 
